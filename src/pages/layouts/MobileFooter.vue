e7c7ff114e0beadec3291078086c01cfa93b8bbb
blkdot <senandev66@gmail.com>
2023-05-09 15:55:16 +0300

    fixed minor issues

--- src/pages/layouts/MobileFooter.vue
+++ src/pages/layouts/MobileFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer :class="`m_md:hidden flex text-${textColor} font-['Handjet'] text-[23px] font-normal font-bold leading-[26px] tracking-[1.6px] flex justify-between w-[300px]`">
+    <footer :class="`flex text-${textColor} font-['Handjet'] text-[23px] font-normal font-bold leading-[26px] tracking-[1.6px] flex justify-between w-[300px]`">
         <a href="https://open-info.gitbook.io/verified-app/" target="_blank" class="block">User Manual</a>
         <a href="https://open-info.gitbook.io/oi-litepaper" target="_blank" class="block">Learn More</a>
     </footer>
