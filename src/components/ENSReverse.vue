a333db81b0e862b2860a9286bbf3ec3dc15ce546
SiaLater69 <siyasanga84@gmail.com>
2023-07-14 21:08:53 +0200

    Create  random number skeleton for ENS alias

--- src/components/ENSReverse.vue
+++ src/components/ENSReverse.vue
@@ -1,20 +1,52 @@
 <template>
+  <div>
+    <div v-if="isLoading">
+      <span class="font-[700]  mr-1">AKA</span><span class="text-gray-500">{{ randomNumbers }}</span>.eth
+    </div>
+    <div v-else>
       <div v-if="alias !== 'no alias'">
         <span class="font-[700]">AKA </span>{{ alias }}
       </div>
       <div v-else>
         no alias
       </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      alias: {
-        type: String,
-        default: 'no alias'
-      }
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    alias: {
+      type: String,
+      default: 'no alias'
     }
-  };
-  </script>
-  
+  },
+  data() {
+    return {
+      isLoading: true,
+      randomNumbers: ''
+    };
+  },
+  created() {
+    // Simulating data fetching delay
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+
+    // Generate random numbers while data is being fetched
+    this.startRandomNumbersCycle();
+  },
+  methods: {
+    startRandomNumbersCycle() {
+      this.randomNumbersInterval = setInterval(() => {
+        this.randomNumbers = Math.floor(Math.random() * 1000000).toString();
+      }, 20);
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.randomNumbersInterval);
+  }
+};
+</script>
+
