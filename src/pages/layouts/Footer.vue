c7a34b868f08fb398b721064e891712641f6a45b
Werner Van der Merwe <werner@Werners-MacBook-Pro.local>
2024-03-01 18:44:00 +0200

    styling update

--- src/pages/layouts/Footer.vue
+++ src/pages/layouts/Footer.vue
@@ -3,42 +3,42 @@
     class="md:hidden flex justify-between items-center px-[50px] w-[100%] h-[147px] border-black border-t-2"
   >
     <div class="text-[23px] leading-[26px] font-[400] text-center">
-      <p :class="`text-${textColor} font-['Ubuntu Condensed']`">Our socials</p>
+      <p :class="`text-${textColor} font-['Handjet']`">Our socials</p>
       <a href="https://twitter.com/_vrfd" target="_blank" class="block">
         <span
-          class="font-[23px] leading-[26px] font-['Ubuntu Condensed'] text-blue"
+          class="font-[23px] leading-[26px] font-['Handjet'] text-blue"
           >Twitter</span
         >
       </a>
       <a href="https://t.me/oi_vrfd" target="_blank" class="block">
         <span
-          class="font-[23px] leading-[26px] font-['Ubuntu Condensed'] text-blue"
+          class="font-[23px] leading-[26px] font-['Handjet'] text-blue"
           >Telegram</span
         >
       </a>
     </div>
     <div class="text-[23px] leading-[26px] font-[400] text-center">
-      <p :class="`text-${textColor} font-['Ubuntu Condensed']`">VRFD v{{ version }}</p>
-      <p :class="`text-${textColor} font-['Ubuntu Condensed']`">
+      <p :class="`text-${textColor} font-['Handjet']`">VRFD v{{ version }}</p>
+      <p :class="`text-${textColor} font-['Handjet']`">
         Free and open source.
       </p>
-      <p :class="`text-${textColor} font-['Ubuntu Condensed']`">
+      <p :class="`text-${textColor} font-['Handjet']`">
         by
         <a href="https://github.com/open-info/" target="_blank">
           <span
-            class="font-[23px] leading-[26px] font-['Ubuntu Condensed'] text-blue"
+            class="font-[23px] leading-[26px] font-['Handjet'] text-blue"
             >Open Info</span
           >.
         </a>
       </p>
     </div>
     <div class="text-[23px] leading-[26px] font-[400] text-center">
-      <p :class="`text-${textColor} font-['Ubuntu Condensed']`">
+      <p :class="`text-${textColor} font-['Handjet']`">
         Saw something weird?
       </p>
       <a href="https://bit.ly/oi-feedback" target="_blank">
         <span
-          class="font-[23px] leading-[26px] font-['Ubuntu Condensed'] text-blue"
+          class="font-[23px] leading-[26px] font-['Handjet'] text-blue"
           >Submit Feedback</span
         >
       </a>
